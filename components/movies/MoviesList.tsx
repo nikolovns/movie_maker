8964532b76ac0e74b662b7fed49751c5cd4c7b77
krasim1 <N3w#W@y_1n>
2023-01-10 14:12:17 +0200

    Change Movie list component render movie items map function

--- components/movies/MoviesList.tsx
+++ components/movies/MoviesList.tsx
@@ -5,19 +5,17 @@ import { MovieItem } from "./MovieItem";
 
 export function MoviesList({ movies }: MovieType): JSX.Element {
 
-	const renderMovies = movies.map((movie: MovieProps) => {
-		return (
-			<MovieItem key={movie.id} {...movie} />
-		)
-	});
-
 	return (
 		<Box sx={{ flexGrow: 1 }}>
 			<Typography variant="h1" sx={{ fontSize: "24px" }}>
 				Movie list
 			</Typography>
 			<Grid container spacing={2}>
-				{renderMovies}
+				{
+					movies.map((movie: MovieProps) => (
+						<MovieItem key={movie.id} {...movie} />
+					))
+				}
 			</Grid>
 		</Box>
 	)
